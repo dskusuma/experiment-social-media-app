50f3095778c3fba6e7cd39f236bcd3fe5ccfa34c
dskusuma <dwisatriakusuma4@gmail.com>
2018-06-12 00:40:36 +0700

    experiment success on fetch posts and displayed in home

--- src/App.js
+++ src/App.js
@@ -6,38 +6,23 @@ import About from './components/About';
 import Contact from './components/Contact';
 import Error from './components/Error';
 import Navigations from './components/Navigations';
+import Posts from './components/Posts';
 
 class App extends Component {
-  getData = async (e) => {
-    e.preventDefault();
-    const postData = fetch('https://jsonplaceholder.typicode.com/posts');
-    const commentData = fetch('https://jsonplaceholder.typicode.com/comments');
-    const userData = fetch('https://jsonplaceholder.typicode.com/users');
-    const data = [
-      await postData.json(),
-      await commentData.json(),
-      await userData.json()
-    ];
-
-    console.log(data);
-  }
   render() {
     return (
-      // <div className="App">
-      // </div>
       <BrowserRouter>
         <div>
           <Navigations />
           <Switch>
-            <Route 
-              path="/" component={Home} exact  
-            />       
+            <Route path="/" component={Home} exact/>       
             <Route path="/about" component={About} />       
             <Route path="/contact" component={Contact} />
+            {/* <Route path="/posts" component={Posts} /> */}
             <Route component={Error} />
-          </Switch>    
+          </Switch>
+          <p>{this.getData}</p>    
         </div>
-        
       </BrowserRouter>
     );
   }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,32 @@
-import React  from 'react';
+import React, { Component } from 'react';
 
-const Home = () => {
-    return(
-    <div>
-        <p> This is Home </p>
-    </div>
-    );
-    
+class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            posts: []
+        }
+    }
+    componentWillMount() {
+        // e.preventDefault();
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then(res => res.json())
+            .then(data => this.setState({ posts: data }));
+    }
+    render() {
+        const postItems = this.state.posts.map(post => (
+            <div key={post.id}>
+                <h3>{post.title}</h3>
+                <p>{post.body}</p> 
+            </div>
+        ));
+
+        return(
+            <div>
+                <h1>Posts</h1>
+                {postItems}
+            </div>   
+        );
+    }
 }
-
 export default Home;
